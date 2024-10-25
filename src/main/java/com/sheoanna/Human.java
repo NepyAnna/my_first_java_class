498db6326daf947348ea96863afe82e77d383ccd
NepyAnna <nepyann@gmail.com>
2024-10-25 22:57:56 +0200

    declarated variables of class

---
+++ src/main/java/com/sheoanna/Human.java
@@ -0,0 +1,9 @@
+package com.sheoanna;
+import java.time.LocalDate;
+
+public class Human {
+    public String name;
+    public String surname;
+    public String ID;
+    public LocalDate dateOfBirth;
+}
